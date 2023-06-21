1629c186e89f577affb44f37b880d6720c4402b6
Kenia <zelbaamadeakenia@gmail.com>
2023-06-21 14:25:30 +0700

    Initial Commit

---
+++ tes/tesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tesApp.swift
+//  tes
+//
+//  Created by Zelba Amadea Kenia on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct tesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
